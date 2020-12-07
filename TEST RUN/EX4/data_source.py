c6537a46b2be873bec911c5e913d1211ed0e6080
ashen007 <asheniranga@gmail.com>
2020-12-07 20:43:34 +0530

    doing

--- TEST RUN/EX4/data_source.py
+++ TEST RUN/EX4/data_source.py
@@ -1,8 +1,19 @@
 import pandas as pd
+from bs4 import BeautifulSoup as bs
 
 
-def read_from_csv(path):
-    dataset = pd.read_csv(path)
+class listener_data():
 
-    return dataset
+    def __init__(self, path):
+        self.__data_points_in_page = []
+        self.__profile = path
 
+    def read_page(self):
+        page = bs(self.__profile)
+
+        return page
+
+
+ashen = listener_data('https://www.last.fm/user/ashen077/library')
+page = ashen.read_page()
+print(len(page))
